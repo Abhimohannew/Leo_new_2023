f2a1d83e4e887f30cb2e75c9f7034b3775d946a3
Master <85671955+abhimohan-tech@users.noreply.github.com>
2023-04-26 07:58:00 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -42,10 +42,10 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     if len(message.command) != 2:
         buttons = [[
-            InlineKeyboardButton('📱 Share With Friends', url='https://t.me/share/url?url=Join%20%40FFMoviesBot%20For%20Download%20New%20Movies')
+            InlineKeyboardButton('Share With Friends 📱', url='https://telegram.me/share/url?url=t.me/FF_Series_Bot')
             ],[
             InlineKeyboardButton('🔍 𝚂𝚎𝚊𝚛𝚌𝚑', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🎬 𝐎𝐓𝐓 𝚄𝙿𝙳𝙰𝚃𝙴𝚂', url='https://t.me/FF_OTT_Updates1')
+            InlineKeyboardButton('🎬 𝐌𝐨𝐯𝐢𝐞𝐬 𝐁𝐨𝐭', url='https://t.me/FFMoviesBot')
             ],[
             InlineKeyboardButton('ℹ️ 𝙷𝚎𝚕𝚙', callback_data='help'),
             InlineKeyboardButton('😊 𝙰𝚋𝚘𝚞𝚝', callback_data='about')
@@ -88,10 +88,10 @@ async def start(client, message):
         return
     if len(message.command) == 2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[
-            InlineKeyboardButton('📱 Share With Friends', url='https://t.me/share/url?url=Join%20%40FFMoviesBot%20For%20Download%20New%20Movies')
+            InlineKeyboardButton('Share With Friends 📱', url='https://telegram.me/share/url?url=t.me/FF_Series_Bot')
             ],[
             InlineKeyboardButton('🔍 𝚂𝚎𝚊𝚛𝚌𝚑', switch_inline_query_current_chat=''),
-            InlineKeyboardButton('🎬 𝐎𝐓𝐓 𝚄𝙿𝙳𝙰𝚃𝙴𝚂', url='https://t.me/FF_OTT_Updates1')
+            InlineKeyboardButton('🎬 𝐌𝐨𝐯𝐢𝐞𝐬 𝐁𝐨𝐭', url='https://t.me/FFMoviesBot')
             ],[
             InlineKeyboardButton('ℹ️ 𝙷𝚎𝚕𝚙', callback_data='help'),
             InlineKeyboardButton('😊 𝙰𝚋𝚘𝚞𝚝', callback_data='about')
